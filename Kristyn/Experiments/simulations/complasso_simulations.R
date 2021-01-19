43f9e90b22822a4cf26be735df570fd750c2b398
Kristyn Pantoja <kristynp@tamu.edu>
2021-01-19 13:56:16 -0600

    fixed complasso simulations -- beta, pred err

--- Kristyn/Experiments/simulations/complasso_simulations.R
+++ Kristyn/Experiments/simulations/complasso_simulations.R
@@ -32,16 +32,15 @@ source("RCode/func_libs.R")
 
 # Method Settings
 tol = 1e-4
-
-# GIC settings
 nlam = 200
 
 # Simulation settings
 numSims = 100
 n = 100
 p = 200
-rho = 0.2
-beta = c(1, 0.4, 1.2, -1.5, -0.8, 0.3, rep(0, p - 6))
+rho = 0.5 # 0.2, 0.5
+# beta = c(1, 0.4, 1.2, -1.5, -0.8, 0.3, rep(0, p - 6))
+beta = c(1, -0.8, 0.6, 0, 0, -1.5, -0.5, 1.2, rep(0, p - 8))
 non0.beta = (beta != 0)
 sigma_eps = 0.5
 seed = 1
@@ -60,7 +59,7 @@ for(i in 1:p){
 # Simulations #
 ################################################################################
 
-# set.seed(1)
+# set.seed(16) # leads to FN = 1
 evals = foreach(
   b = 1:numSims, 
   .combine = cbind, 
@@ -73,6 +72,8 @@ evals = foreach(
   library(compositions)
   library(stats)
   source("RCode/func_libs.R")
+  
+  nlam = 200
   
   # simulate data
   # generate W
@@ -87,25 +88,26 @@ evals = foreach(
   Y = Z %*% matrix(beta) + epsilon
   
   # apply compositional lasso, using GIC to select lambda
-  complasso = ConstrLasso(
-    Y, Z, Cmat = matrix(1, p, 1), 
-    nlam = nlam, tol = tol)
-  non0.betahats = (complasso$bet != 0) # diff lambda = diff col
+  complasso0 = ConstrLasso(Y, Z, Cmat = matrix(1, p, 1), nlam = nlam, tol = tol)
+  non0.betahats = (complasso0$bet != 0) # diff lambda = diff col
   non0ct = apply(non0.betahats, 2, sum) # count of non-zero betas for each lambda
   which0ct.leq3sqrtn = which(non0ct <= 3 * sqrt(n)) # lambda lower bound criteria
-  lambda = complasso$lambda[which0ct.leq3sqrtn]
+  lambda = seq(complasso0$lambda[1], 
+               complasso0$lambda[max(which0ct.leq3sqrtn)], 
+               length.out = nlam)
+  complasso = ConstrLasso(Y, Z, Cmat = matrix(1, p, 1), lambda = lambda, 
+                          nlam = nlam, tol = tol)
   nlam = length(lambda)
-  non0.betahats = non0.betahats[, which0ct.leq3sqrtn]
-  non0ct = non0ct[which0ct.leq3sqrtn]
-  betahats = complasso$bet[, which0ct.leq3sqrtn]
-  a0s = complasso$int[which0ct.leq3sqrtn]
+  non0.betahats = (complasso$bet != 0)
+  non0ct = apply(non0.betahats, 2, sum)
+  # which0ct.leq3sqrtn = which(non0ct <= 3 * sqrt(n))
+  a0s = complasso$int
+  betahats = complasso$bet
   GIC = rep(NA, nlam)
-  PE = rep(NA, nlam)
   for(m in 1:nlam){
     a0.tmp = a0s[m]
     betahat.tmp = betahats[, m]
     sigmasq.hat = crossprod(Y - a0.tmp - Z %*% betahat.tmp) / n
-    PE[m] = sigmasq.hat
     pvn = p
     s.lam = sum(non0.betahats[, m])
     GIC[m] = log(sigmasq.hat) + (s.lam - 1) * log(log(n)) * log(pvn) / n
@@ -116,18 +118,22 @@ evals = foreach(
   betahat = complasso$bet[, lam.min.idx]
   non0.betahat = non0.betahats[, lam.min.idx]
   
-  # begin plots
-  plot(lambda, GIC, type = "l")
-  points(x = lam.min, GIC[lam.min.idx])
-  text(x = lam.min, y = GIC[lam.min.idx], pos = 4,
-       labels = paste0("(", round(lam.min, 3), ",", round(GIC[lam.min.idx], 3), ")"))
-  # data.frame(lam = lambda, gic = GIC, non0s = non0ct)
-  plot(lambda, non0ct, type = "l")
-  # end plots
+  # plot(complasso$lambda, GIC, type = "l")
   
   # evaluate model
   # prediction error
-  PE = PE[lam.min.idx]
+  # simulate independent test set of size n
+  # generate W
+  W.test = rmvnorm(n = n, mean = muW, sigma = SigmaW) # n x p
+  # let X = exp(w_ij) / (sum_k=1:p w_ik) ~ Logistic Normal (the covariates)
+  V.test = exp(W.test)
+  rowsumsV.test = apply(V.test, 1, sum)
+  X.test = V.test / rowsumsV.test
+  epsilon.test = rnorm(n, 0, sigma_eps)
+  Z.test = log(X.test)
+  # generate Y
+  Y.test = Z.test %*% matrix(beta) + epsilon.test
+  PE = crossprod(Y.test - a0 - Z.test %*% betahat) / n
   # estimation accuracy
   EA1 = sum(abs(betahat - beta))
   EA2 = crossprod(betahat - beta)
@@ -139,26 +145,23 @@ evals = foreach(
   # return
   c(PE, EA1, EA2, EAInfty, FP, FN)
 }
-rownames(bs.selected_variables) = colnames(X)
-
-bs.selected_variables_numeric = apply(bs.selected_variables, 2, as.numeric)
-bs.selection_percentages = apply(bs.selected_variables_numeric, 1, FUN = 
-                                   function(x) sum(x, na.rm = TRUE) / bs.n)
-names(bs.selection_percentages) = rownames(bs.selected_variables)
-bs.results = list(
-  seed = rng.seed,  
-  selected_variables = bs.selected_variables, 
-  selection_percentages = bs.selection_percentages
-)
-
-saveRDS(bs.results,
+rownames(evals) = c("PE", "EA1", "EA2", "EAInfty", "FP", "FN")
+saveRDS(evals,
         file = paste0("Kristyn/Experiments/output",
-                      "/complasso_selection", 
-                      "_refit",
-                      "_B", bs.n, 
+                      "/complasso_simulations", 
+                      "_dim", n, "x", p, 
+                      "_rho", rho, 
                       "_seed", rng.seed,
                       ".rds"))
-
-sort(bs.selection_percentages)
-
-
+eval.means = apply(evals, 1, mean)
+eval.sds = apply(evals, 1, sd)
+eval.ses = eval.sds / sqrt(numSims)
+evals.df = data.frame("mean" = eval.means, "sd" = eval.sds, "se" = eval.ses)
+evals.df
+saveRDS(evals.df,
+        file = paste0("Kristyn/Experiments/output",
+                      "/complasso_simulations_summary", 
+                      "_dim", n, "x", p, 
+                      "_rho", rho, 
+                      "_seed", rng.seed,
+                      ".rds"))

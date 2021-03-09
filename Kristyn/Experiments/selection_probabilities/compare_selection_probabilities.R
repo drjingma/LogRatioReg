2b42514e9101efabf76ce0fb5919622685aa756c
Kristyn Pantoja <kristynp@tamu.edu>
2021-03-08 21:22:01 -0600

    fix selection probabilities comparison script

--- Kristyn/Experiments/selection_probabilities/compare_selection_probabilities.R
+++ Kristyn/Experiments/selection_probabilities/compare_selection_probabilities.R
@@ -1,31 +1,57 @@
 output_home = "Kristyn/Experiments/selection_probabilities/output/"
+rng.seed = 123
+bs.n = 500
+intercept = TRUE
 ################################################################################
 # Compositional Lasso #
 ################################################################################
 
-complasso = readRDS(paste0(output_home, "complasso_selection_refit_seed123.rds"))
-length(complasso$selection_percentages)
-dim(complasso$selected_variables)
-all(!is.na(complasso$selected_variables)) # no NAs
-sort(complasso$selection_percentages)
+complasso = readRDS(paste0(
+  output_home,
+  "complasso_selection_refit", 
+  "_int", intercept, 
+  "_B", bs.n, 
+  "_seed", rng.seed,
+  ".rds"
+))
+# length(complasso$selection_percentages)
+# dim(complasso$selected_variables)
+# all(!is.na(complasso$selected_variables)) # no NAs
+head(sort(complasso$selection_percentages, decreasing = TRUE), 5)
 
 ################################################################################
 # Supervised Log Ratios - refit Log Ratios #
 ################################################################################
 
-slrLR = readRDS(paste0(output_home, "slr_selection_refitLRs_seed123.rds"))
-length(slrLR$selection_percentages)
-dim(slrLR$selected_variables)
-all(!is.na(slrLR$selected_variables)) # no NAs
-sort(slrLR$selection_percentages)
+slrLR = readRDS(paste0(
+  output_home,
+  "slr_selection", 
+  "_refitLRs",
+  "_int", intercept, 
+  "_B", bs.n, 
+  "_seed", rng.seed,
+  ".rds"
+))
+# length(slrLR$selection_percentages)
+# dim(slrLR$selected_variables)
+# all(!is.na(slrLR$selected_variables)) # no NAs
+head(sort(slrLR$selection_percentages, decreasing = TRUE), 5)
 
 ################################################################################
 # Supervised Log Ratios - refit Log Contrasts #
 ################################################################################
 
-slrLC = readRDS(paste0(output_home, "slr_selection_refitLCs_seed123.rds"))
-length(slrLC$selection_percentages)
-dim(slrLC$selected_variables)
-all(!is.na(slrLC$selected_variables)) # no NAs
-sort(slrLC$selection_percentages)
+slrLC = readRDS(paste0(
+  output_home,
+  "slr_selection", 
+  "_refitLCs",
+  "_int", intercept, 
+  "_B", bs.n, 
+  "_seed", rng.seed,
+  ".rds"
+))
+# length(slrLC$selection_percentages)
+# dim(slrLC$selected_variables)
+# all(!is.na(slrLC$selected_variables)) # no NAs
+head(sort(slrLC$selection_percentages, decreasing = TRUE), 5)
 

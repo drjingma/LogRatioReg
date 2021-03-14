f0e2f8329259a97a0b13b13ed77f57230a7cfa50
Kristyn Pantoja <kristynp@z11.stat.tamu.edu>
2021-03-13 20:45:27 -0600

    cluster sims

--- Kristyn/Experiments/slr_simulations/complasso_simulations_PBAbtree_cv.R
+++ Kristyn/Experiments/slr_simulations/complasso_simulations_PBAbtree_cv.R
@@ -41,10 +41,10 @@ K = 5
 
 # Simulation settings
 numSims = 100
-n = 50
-p = 30
+n = 100
+p = 200
 rho = 0.2 # 0.2, 0.5
-generate.theta = 1 # 1 = sparse beta, 2 = not-sparse beta
+generate.theta = 2 # 1 = sparse beta, 2 = not-sparse beta
 sigma_eps = 0.5
 seed = 1
 muW = c(

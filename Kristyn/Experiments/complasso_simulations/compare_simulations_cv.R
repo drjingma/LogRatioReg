eeefdc2e07a74ba29cb8dadc4a7502bd4fb5e437
Kristyn Pantoja <kristynp@tamu.edu>
2021-04-06 13:29:22 -0500

    comparing lin et al simulations results

--- Kristyn/Experiments/complasso_simulations/compare_simulations_cv.R
+++ Kristyn/Experiments/complasso_simulations/compare_simulations_cv.R
@@ -1,36 +1,73 @@
+library(ggplot2)
+library(reshape2)
+
 output_dir = "Kristyn/Experiments/complasso_simulations/output"
 rng.seed = 123
-n = 100
-p = 200
+n = 50
+p = 30
 rho = 0.2 # 0.2, 0.5
 intercept = TRUE
 
 ################################################################################
 # Compositional Lasso #
 ################################################################################
-
-complasso = readRDS(paste0(
+complasso.sims = readRDS(paste0(
   output_dir,
-  "/complasso_cv_simulations", 
+  "/complasso_cv_sims", 
   "_dim", n, "x", p, 
   "_rho", rho, 
   "_int", intercept, 
   "_seed", rng.seed,
   ".rds"
 ))
-print(complasso[, c("mean", "se")])
+complasso.summaries = readRDS(paste0(
+  output_dir,
+  "/complasso_cv_summaries", 
+  "_dim", n, "x", p, 
+  "_rho", rho, 
+  "_int", intercept, 
+  "_seed", rng.seed,
+  ".rds"
+))
+print(complasso.summaries[, c("mean", "se")])
 
 ################################################################################
 # Supervised Log Ratios #
 ################################################################################
-slr.lr = readRDS(paste0(
+slr.sims = readRDS(paste0(
   output_dir,
-  "/slr_cv_simulations", 
+  "/slr_cv_sims", 
   "_dim", n, "x", p, 
   "_rho", rho, 
   "_int", intercept, 
   "_seed", rng.seed,
   ".rds"
 ))
-print(slr.lr[, c("mean", "se")])
+slr.summaries = readRDS(paste0(
+  output_dir,
+  "/slr_cv_summaries", 
+  "_dim", n, "x", p, 
+  "_rho", rho, 
+  "_int", intercept, 
+  "_seed", rng.seed,
+  ".rds"
+))
+print(slr.summaries[, c("mean", "se")])
 
+
+
+
+# plot
+
+cl.sims.gg = melt(data.frame(t(complasso.sims)))
+cl.sims.gg$type = "CompLasso"
+slr.sims.gg = melt(data.frame(t(slr.sims)))
+slr.sims.gg$type = "SLR"
+data.gg = rbind(cl.sims.gg, slr.sims.gg)
+data.gg$type = factor(data.gg$type, levels = c("CompLasso", "SLR"))
+ggplot(data.gg, aes(x = type, y = value, color = type)) + 
+  facet_wrap(vars(variable), scales = "free_y") + geom_boxplot() + 
+  stat_summary(fun = mean, geom = "point", shape = 17, size = 2, color = "red") +
+  theme_bw() + 
+  theme(axis.title.x = element_blank(), 
+        axis.title.y = element_blank())

39879e82c07586fbd44e77b968a26c9fd248252c
kristynpantoja <kristynp@tamu.edu>
2022-02-09 01:12:40 -0600

    sims

--- Kristyn/Experiments/balancereg_hsclust_experiments/plot_metrics_Rsquared.R
+++ Kristyn/Experiments/balancereg_hsclust_experiments/plot_metrics_Rsquared.R
@@ -44,7 +44,7 @@ get_sigma_eps = function(theta_val, Rsq_val, rho_val){
   return(sqrt(sigma_eps_sq.tmp))
 }
 rho = 0.2 #
-desired_Rsquared = 0.6 #
+desired_Rsquared = 0.8 #
 sigma_eps = get_sigma_eps(
   theta_val = values.theta, Rsq_val = desired_Rsquared, rho_val = rho)
 
@@ -63,19 +63,18 @@ metric_names = c(
 
 # import metrics
 slrhc_sims_list = list()
-slrhc2_sims_list = list()
 slrhc_distal_sims_list = list()
 slrhsc_sims_list = list()
-slrhsc2_sims_list = list()
-# slr2hsc2_sims_list = list()
-slrhsc_natstop_sims_list = list()
-slrhsc_ngmstop_sims_list = list()
+slrhsc_thresh_lasso_sims_list = list()
+slrhsc_thresh_mlm_sims_list = list()
+slrhsc_thresh_1lm_sims_list = list()
 cl_sims_list = list()
 pr_sims_list = list()
 or_sims_list = list()
 # slbl_sims_list = list()
 for(i in 1:numSims){
   print(i)
+  
   # slr hc
   slrhc.sim.tmp = t(data.frame(readRDS(paste0(
     output_dir, "/metrics", "/slr_hc_", "metrics", file.end0,
@@ -83,20 +82,15 @@ for(i in 1:numSims){
   ))))
   rownames(slrhc.sim.tmp) = NULL
   slrhc_sims_list[[i]] = data.table(slrhc.sim.tmp)
-  # slr hc - eta
-  slrhc2.sim.tmp = t(data.frame(readRDS(paste0(
-    output_dir, "/metrics", "/slr_hc_eta_", "metrics", file.end0,
-    "_sim", i, ".rds"
-  ))))
-  rownames(slrhc2.sim.tmp) = NULL
-  slrhc2_sims_list[[i]] = data.table(slrhc2.sim.tmp)
-  # slr hc - eta
+  
+  # slr hc distal
   slrhc_distal.sim.tmp = t(data.frame(readRDS(paste0(
     output_dir, "/metrics", "/slr_hc_distal_", "metrics", file.end0,
     "_sim", i, ".rds"
   ))))
   rownames(slrhc_distal.sim.tmp) = NULL
   slrhc_distal_sims_list[[i]] = data.table(slrhc_distal.sim.tmp)
+  
   # slr hsc
   slrhsc.sim.tmp = t(data.frame(readRDS(paste0(
     output_dir, "/metrics", "/slr_hsc_", "metrics", file.end0,
@@ -104,34 +98,31 @@ for(i in 1:numSims){
   ))))
   rownames(slrhsc.sim.tmp) = NULL
   slrhsc_sims_list[[i]] = data.table(slrhsc.sim.tmp)
-  # slr hsc - eta
-  slrhsc2.sim.tmp = t(data.frame(readRDS(paste0(
-    output_dir, "/metrics", "/slr_hsc_eta_", "metrics", file.end0,
+  
+  # slr hsc thresh lasso
+  slrhsc_thresh_lasso.sim.tmp = t(data.frame(readRDS(paste0(
+    output_dir, "/metrics", "/slr_hsc_thresh_lasso_", "metrics", file.end0,
     "_sim", i, ".rds"
   ))))
-  rownames(slrhsc2.sim.tmp) = NULL
-  slrhsc2_sims_list[[i]] = data.table(slrhsc2.sim.tmp)
-  # # slr2 hsc - eta
-  # slr2hsc2.sim.tmp = t(data.frame(readRDS(paste0(
-  #   output_dir, "/metrics", "/slr2_hsc_eta_", "metrics", file.end0,
-  #   "_sim", i, ".rds"
-  # ))))
-  # rownames(slr2hsc2.sim.tmp) = NULL
-  # slr2hsc2_sims_list[[i]] = data.table(slr2hsc2.sim.tmp)
-  # slr hsc - eta - natural stop
-  slrhsc_natstop.sim.tmp = t(data.frame(readRDS(paste0(
-    output_dir, "/metrics", "/slr_hsc_eta_natstop_", "metrics", file.end0,
+  rownames(slrhsc_thresh_lasso.sim.tmp) = NULL
+  slrhsc_thresh_lasso_sims_list[[i]] = data.table(slrhsc_thresh_lasso.sim.tmp)
+  
+  # slr hsc thresh mult. lm
+  slrhsc_thresh_mlm.sim.tmp = t(data.frame(readRDS(paste0(
+    output_dir, "/metrics", "/slr_hsc_thresh_mlm_", "metrics", file.end0,
     "_sim", i, ".rds"
   ))))
-  rownames(slrhsc_natstop.sim.tmp) = NULL
-  slrhsc_natstop_sims_list[[i]] = data.table(slrhsc_natstop.sim.tmp)
-  # # slr hsc - eta - NGM stop
-  slrhsc_ngmstop.sim.tmp = t(data.frame(readRDS(paste0(
-    output_dir, "/metrics", "/slr_hsc_eta_ngmstop_", "metrics", file.end0,
+  rownames(slrhsc_thresh_mlm.sim.tmp) = NULL
+  slrhsc_thresh_mlm_sims_list[[i]] = data.table(slrhsc_thresh_mlm.sim.tmp)
+  
+  # slr hsc thresh single lm
+  slrhsc_thresh_1lm.sim.tmp = t(data.frame(readRDS(paste0(
+    output_dir, "/metrics", "/slr_hsc_thresh_1lm_", "metrics", file.end0,
     "_sim", i, ".rds"
   ))))
-  rownames(slrhsc_ngmstop.sim.tmp) = NULL
-  slrhsc_ngmstop_sims_list[[i]] = data.table(slrhsc_ngmstop.sim.tmp)
+  rownames(slrhsc_thresh_1lm.sim.tmp) = NULL
+  slrhsc_thresh_1lm_sims_list[[i]] = data.table(slrhsc_thresh_1lm.sim.tmp)
+  
   # classo
   cl.sim.tmp = t(data.frame(readRDS(paste0(
     output_dir, "/metrics", "/classo_", "metrics", file.end0,
@@ -139,6 +130,7 @@ for(i in 1:numSims){
   ))))
   rownames(cl.sim.tmp) = NULL
   cl_sims_list[[i]] = data.table(cl.sim.tmp)
+  
   # propr
   pr.sim.tmp = t(data.frame(readRDS(paste0(
     output_dir, "/metrics", "/propr_", "metrics", file.end0,
@@ -146,6 +138,7 @@ for(i in 1:numSims){
   ))))
   rownames(pr.sim.tmp) = NULL
   pr_sims_list[[i]] = data.table(pr.sim.tmp)
+  
   # oracle
   or.sim.tmp = t(data.frame(readRDS(paste0(
     output_dir, "/metrics", "/oracle_", "metrics", file.end0,
@@ -153,6 +146,7 @@ for(i in 1:numSims){
   ))))
   rownames(or.sim.tmp) = NULL
   or_sims_list[[i]] = data.table(or.sim.tmp)
+  
   # # selbal
   # slbl.sim.tmp = t(data.frame(readRDS(paste0(
   #   output_dir, "/metrics", "/selbal_", "metrics", file.end0,
@@ -163,27 +157,21 @@ for(i in 1:numSims){
 }
 
 slrhc_sims = as.data.frame(rbindlist(slrhc_sims_list))
-slrhc2_sims = as.data.frame(rbindlist(slrhc2_sims_list))
 slrhc_distal_sims = as.data.frame(rbindlist(slrhc_distal_sims_list))
 slrhsc_sims = as.data.frame(rbindlist(slrhsc_sims_list))
-slrhsc2_sims = as.data.frame(rbindlist(slrhsc2_sims_list))
-slrhsc_natstop_sims = as.data.frame(rbindlist(slrhsc_natstop_sims_list))
-slrhsc_ngmstop_sims = as.data.frame(rbindlist(slrhsc_ngmstop_sims_list))
+slrhsc_thresh_lasso_sims = as.data.frame(rbindlist(slrhsc_thresh_lasso_sims_list))
+slrhsc_thresh_mlm_sims = as.data.frame(rbindlist(slrhsc_thresh_mlm_sims_list))
+slrhsc_thresh_1lm_sims = as.data.frame(rbindlist(slrhsc_thresh_1lm_sims_list))
 cl_sims = as.data.frame(rbindlist(cl_sims_list))
 pr_sims = as.data.frame(rbindlist(pr_sims_list))
 or_sims = as.data.frame(rbindlist(or_sims_list))
-slbl_sims = as.data.frame(rbindlist(slbl_sims_list))
+# slbl_sims = as.data.frame(rbindlist(slbl_sims_list))
 
 # summary stats
 slrhc_summaries = data.frame(
   "mean" = apply(slrhc_sims, 2, mean), 
   "sd" = apply(slrhc_sims, 2, sd), 
   "se" =  apply(slrhc_sims, 2, sd) / sqrt(numSims)
-)
-slrhc2_summaries = data.frame(
-  "mean" = apply(slrhc2_sims, 2, mean), 
-  "sd" = apply(slrhc2_sims, 2, sd), 
-  "se" =  apply(slrhc2_sims, 2, sd) / sqrt(numSims)
 )
 slrhc_distal_summaries = data.frame(
   "mean" = apply(slrhc_distal_sims, 2, mean), 
@@ -195,20 +183,20 @@ slrhsc_summaries = data.frame(
   "sd" = apply(slrhsc_sims, 2, sd), 
   "se" =  apply(slrhsc_sims, 2, sd) / sqrt(numSims)
 )
-slrhsc2_summaries = data.frame(
-  "mean" = apply(slrhsc2_sims, 2, mean), 
-  "sd" = apply(slrhsc2_sims, 2, sd), 
-  "se" =  apply(slrhsc2_sims, 2, sd) / sqrt(numSims)
+slrhsc_thresh_lasso_summaries = data.frame(
+  "mean" = apply(slrhsc_thresh_lasso_sims, 2, mean), 
+  "sd" = apply(slrhsc_thresh_lasso_sims, 2, sd), 
+  "se" =  apply(slrhsc_thresh_lasso_sims, 2, sd) / sqrt(numSims)
 )
-slrhsc_natstop_summaries = data.frame(
-  "mean" = apply(slrhsc_natstop_sims, 2, mean), 
-  "sd" = apply(slrhsc_natstop_sims, 2, sd), 
-  "se" =  apply(slrhsc_natstop_sims, 2, sd) / sqrt(numSims)
+slrhsc_thresh_mlm_summaries = data.frame(
+  "mean" = apply(slrhsc_thresh_mlm_sims, 2, mean), 
+  "sd" = apply(slrhsc_thresh_mlm_sims, 2, sd), 
+  "se" =  apply(slrhsc_thresh_mlm_sims, 2, sd) / sqrt(numSims)
 )
-slrhsc_ngmstop_summaries = data.frame(
-  "mean" = apply(slrhsc_ngmstop_sims, 2, mean),
-  "sd" = apply(slrhsc_ngmstop_sims, 2, sd),
-  "se" =  apply(slrhsc_ngmstop_sims, 2, sd) / sqrt(numSims)
+slrhsc_thresh_1lm_summaries = data.frame(
+  "mean" = apply(slrhsc_thresh_1lm_sims, 2, mean), 
+  "sd" = apply(slrhsc_thresh_1lm_sims, 2, sd), 
+  "se" =  apply(slrhsc_thresh_1lm_sims, 2, sd) / sqrt(numSims)
 )
 cl_summaries = data.frame(
   "mean" = apply(cl_sims, 2, mean), 
@@ -225,48 +213,45 @@ or_summaries = data.frame(
   "sd" = apply(or_sims, 2, sd), 
   "se" =  apply(or_sims, 2, sd) / sqrt(numSims)
 )
-slbl_summaries = data.frame(
-  "mean" = apply(slbl_sims, 2, mean), 
-  "sd" = apply(slbl_sims, 2, sd), 
-  "se" =  apply(slbl_sims, 2, sd) / sqrt(numSims)
-)
+# slbl_summaries = data.frame(
+#   "mean" = apply(slbl_sims, 2, mean), 
+#   "sd" = apply(slbl_sims, 2, sd), 
+#   "se" =  apply(slbl_sims, 2, sd) / sqrt(numSims)
+# )
 
 # metrics boxplots
 slrhc.sims.gg = reshape2::melt(slrhc_sims)
 slrhc.sims.gg$Method = "slr-hc"
-slrhc2.sims.gg = reshape2::melt(slrhc2_sims)
-slrhc2.sims.gg$Method = "slr-hc-eta"
 slrhc_distal.sims.gg = reshape2::melt(slrhc_distal_sims)
 slrhc_distal.sims.gg$Method = "slr-hc-distal"
 slrhsc.sims.gg = reshape2::melt(slrhsc_sims)
 slrhsc.sims.gg$Method = "slr-hsc"
-slrhsc2.sims.gg = reshape2::melt(slrhsc2_sims)
-slrhsc2.sims.gg$Method = "slr-hsc-eta"
-slrhsc_natstop.sims.gg = reshape2::melt(slrhsc_natstop_sims)
-slrhsc_natstop.sims.gg$Method = "slr-hsc-eta-nat"
-slrhsc_ngmstop.sims.gg = reshape2::melt(slrhsc_ngmstop_sims)
-slrhsc_ngmstop.sims.gg$Method = "slr-hsc-eta-ngm"
+slrhsc_thresh_lasso.sims.gg = reshape2::melt(slrhsc_thresh_lasso_sims)
+slrhsc_thresh_lasso.sims.gg$Method = "slr-hsc-thresh-lasso"
+slrhsc_thresh_mlm.sims.gg = reshape2::melt(slrhsc_thresh_mlm_sims)
+slrhsc_thresh_mlm.sims.gg$Method = "slr-hsc-thresh-mlm"
+slrhsc_thresh_1lm.sims.gg = reshape2::melt(slrhsc_thresh_1lm_sims)
+slrhsc_thresh_1lm.sims.gg$Method = "slr-hsc-thresh-1lm"
 cl.sims.gg = reshape2::melt(cl_sims)
 cl.sims.gg$Method = "classo"
 pr.sims.gg = reshape2::melt(pr_sims)
 pr.sims.gg$Method = "propr"
 or.sims.gg = reshape2::melt(or_sims)
 or.sims.gg$Method = "oracle (hc)"
-slbl.sims.gg = reshape2::melt(slbl_sims)
-slbl.sims.gg$Method = "selbal"
+# slbl.sims.gg = reshape2::melt(slbl_sims)
+# slbl.sims.gg$Method = "selbal"
 
 data.gg0 = rbind(
   slrhc.sims.gg, 
-  # slrhc2.sims.gg, # slr-hc-eta -- not good
   slrhc_distal.sims.gg,
-  slrhsc.sims.gg, # slr-hsc -- not good, if not thresholding
-  slrhsc2.sims.gg, 
-  slrhsc_natstop.sims.gg, 
-  slrhsc_ngmstop.sims.gg,
+  slrhsc.sims.gg,
+  slrhsc_thresh_lasso.sims.gg, 
+  slrhsc_thresh_mlm.sims.gg, 
+  slrhsc_thresh_1lm.sims.gg,
   cl.sims.gg, 
   pr.sims.gg, 
-  or.sims.gg, 
-  slbl.sims.gg)
+  or.sims.gg)#, 
+  # slbl.sims.gg)
 # levels.gg = c(
 #   "slr-hc", "slr-hsc", "slr-hc-eta", "slr-hsc-eta", "classo", "propr")
 data.gg = data.gg0
@@ -302,7 +287,7 @@ ggplot(data.gg, aes(x = Method, y = value, color = Method)) +
 
 ggsave(
   filename = paste0(
-    "20220125",
+    "20220208",
     "_Rsq", desired_Rsquared,
     "_rho", rho, 
     "_", "metrics", ".pdf"),

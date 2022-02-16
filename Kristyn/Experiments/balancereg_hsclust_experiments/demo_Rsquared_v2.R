2f508ba4558fed41fb2b096f060d24b829fb2314
kristynpantoja <kristynp@tamu.edu>
2022-02-16 12:22:26 -0600

    fixed slr fn

--- Kristyn/Experiments/balancereg_hsclust_experiments/demo_Rsquared_v2.R
+++ Kristyn/Experiments/balancereg_hsclust_experiments/demo_Rsquared_v2.R
@@ -117,88 +117,88 @@ SSres = sum((y.all - as.numeric(log(X.all) %*% beta))^2)
 SStot = sum((y.all - mean(y.all))^2)
 Rsq = 1 - SSres/SStot
 
-##############################################################################
-# supervised log-ratios (a balance regression method) with eta
-#   -- hierarchical spectral clustering
-##############################################################################
-# apply hierarchical spectral clustering to the SLR similarity matrix
-slrSimMat = getSlrMatrix(
-  y = Y, X = X, type = "similarity")
-
-# kmeans
-slrhsc_btree = HSClust(
-  W = slrSimMat, force_levelMax = TRUE, method = "kmeans")
-slrhsc_SBP = sbp.fromHSClust(
-  levels_matrix = slrhsc_btree$allLevels, row_names = names(beta))
-# plotSBP(slrhsc_SBP)
-fields::image.plot(slrSimMat)
-
-# # shi-malik
-# slrhsc_btree2 = HSClust(
-#   W = slrSimMat, force_levelMax = TRUE, method = "shimalik")
-# slrhsc_SBP2 = sbp.fromHSClust(
-#   levels_matrix = slrhsc_btree2$allLevels, row_names = names(beta))
-# plotSBP(slrhsc_SBP2)
-
-# apply supervised log-ratios, using CV to select threshold
-slrhsc2 = cvBMLassoThresh(
-  y = Y, X = X,
-  W = slrSimMat, # normalized similarity matrix (all values between 0 & 1)
-  hsc_method = "kmeans", # "shimalik", "kmeans"
-  # thresh_method = "original",
-  # multiple_balances = TRUE,
-  force_levelMax = TRUE,
-  stopping_rule = NULL,
-  sbp = slrhsc_SBP,
-  neta = neta, nlam = nlam,
-  nfolds = K, foldid = NULL,
-  intercept = intercept,
-  standardize = scaling,
-  prints = TRUE,
-  seed = 123
-)
-
-slrhsc2.eta.min.idx = slrhsc2$min.idx[2]
-slrhsc2.lam.min.idx = slrhsc2$min.idx[1]
-slrhsc2.a0 = slrhsc2$theta0[[slrhsc2.eta.min.idx]][slrhsc2.lam.min.idx]
-slrhsc2.thetahat = slrhsc2$theta[[slrhsc2.eta.min.idx]][, slrhsc2.lam.min.idx]
-slrhsc2.SBP = slrhsc2$sbp_thresh[[slrhsc2.eta.min.idx]]
-slrhsc2.betahat.nonzero = getBetaFromTheta(slrhsc2.thetahat, sbp = slrhsc2.SBP)
-slrhsc2.betahat = matrix(0, nrow = ncol(X), ncol = 1)
-rownames(slrhsc2.betahat) = names(beta)
-slrhsc2.betahat[slrhsc2$meets_threshold[[slrhsc2.eta.min.idx]], ] =
-  as.numeric(slrhsc2.betahat.nonzero)
-slrhsc2.betahat
-
-# compute metrics on the selected model #
-slrhsc2.metrics = getMetricsBalanceReg(
-  y.train = Y, y.test = Y.test,
-  ilrX.train = getIlrX(
-    X[, slrhsc2$meets_threshold[[slrhsc2.eta.min.idx]], drop = FALSE],
-    sbp = slrhsc2.SBP),
-  ilrX.test = getIlrX(
-    X.test[, slrhsc2$meets_threshold[[slrhsc2.eta.min.idx]], drop = FALSE],
-    sbp = slrhsc2.SBP),
-  n.train = n, n.test = n,
-  thetahat0 = slrhsc2.a0, thetahat = slrhsc2.thetahat,
-  betahat = slrhsc2.betahat,
-  sbp = slrhsc2.SBP,
-  true.beta = beta, is0.true.beta = is0.beta, non0.true.beta = non0.beta)
-
-# plot the tree given by slr-hsc, indicating significant covariates
-slrhsc2_leaf_types = rep("covariate", nrow(slrhsc2.SBP))
-slrhsc2_balance_types = rep("balance", ncol(slrhsc2.SBP))
-slrhsc2_nodes_types = data.frame(
-  name = c(colnames(slrhsc2.SBP), rownames(slrhsc2.SBP)),
-  type = c(slrhsc2_balance_types, slrhsc2_leaf_types)
-)
-plotSBP(slrhsc2.SBP, title = "slr-hsc-eta (lasso)", nodes_types = slrhsc2_nodes_types)
-
-# note: centering X and y didn't change the slrSimMat
-# note 2: using clrXj - clrXk instead of logXj - logXk didn't change slrSimMat
-
-# heat map of cross-validated mse's
-# fields::image.plot(slrhsc2$cvm)
+# ##############################################################################
+# # supervised log-ratios (a balance regression method) with eta
+# #   -- hierarchical spectral clustering
+# ##############################################################################
+# # apply hierarchical spectral clustering to the SLR similarity matrix
+# slrSimMat = getSlrMatrix(
+#   y = Y, X = X, type = "similarity")
+# 
+# # kmeans
+# slrhsc_btree = HSClust(
+#   W = slrSimMat, force_levelMax = TRUE, method = "kmeans")
+# slrhsc_SBP = sbp.fromHSClust(
+#   levels_matrix = slrhsc_btree$allLevels, row_names = names(beta))
+# # plotSBP(slrhsc_SBP)
+# fields::image.plot(slrSimMat)
+# 
+# # # shi-malik
+# # slrhsc_btree2 = HSClust(
+# #   W = slrSimMat, force_levelMax = TRUE, method = "shimalik")
+# # slrhsc_SBP2 = sbp.fromHSClust(
+# #   levels_matrix = slrhsc_btree2$allLevels, row_names = names(beta))
+# # plotSBP(slrhsc_SBP2)
+# 
+# # apply supervised log-ratios, using CV to select threshold
+# slrhsc2 = cvBMLassoThresh(
+#   y = Y, X = X,
+#   W = slrSimMat, # normalized similarity matrix (all values between 0 & 1)
+#   hsc_method = "kmeans", # "shimalik", "kmeans"
+#   # thresh_method = "original",
+#   # multiple_balances = TRUE,
+#   force_levelMax = TRUE,
+#   stopping_rule = NULL,
+#   sbp = slrhsc_SBP,
+#   neta = neta, nlam = nlam,
+#   nfolds = K, foldid = NULL,
+#   intercept = intercept,
+#   standardize = scaling,
+#   prints = TRUE,
+#   seed = 123
+# )
+# 
+# slrhsc2.eta.min.idx = slrhsc2$min.idx[2]
+# slrhsc2.lam.min.idx = slrhsc2$min.idx[1]
+# slrhsc2.a0 = slrhsc2$theta0[[slrhsc2.eta.min.idx]][slrhsc2.lam.min.idx]
+# slrhsc2.thetahat = slrhsc2$theta[[slrhsc2.eta.min.idx]][, slrhsc2.lam.min.idx]
+# slrhsc2.SBP = slrhsc2$sbp_thresh[[slrhsc2.eta.min.idx]]
+# slrhsc2.betahat.nonzero = getBetaFromTheta(slrhsc2.thetahat, sbp = slrhsc2.SBP)
+# slrhsc2.betahat = matrix(0, nrow = ncol(X), ncol = 1)
+# rownames(slrhsc2.betahat) = names(beta)
+# slrhsc2.betahat[slrhsc2$meets_threshold[[slrhsc2.eta.min.idx]], ] =
+#   as.numeric(slrhsc2.betahat.nonzero)
+# slrhsc2.betahat
+# 
+# # compute metrics on the selected model #
+# slrhsc2.metrics = getMetricsBalanceReg(
+#   y.train = Y, y.test = Y.test,
+#   ilrX.train = getIlrX(
+#     X[, slrhsc2$meets_threshold[[slrhsc2.eta.min.idx]], drop = FALSE],
+#     sbp = slrhsc2.SBP),
+#   ilrX.test = getIlrX(
+#     X.test[, slrhsc2$meets_threshold[[slrhsc2.eta.min.idx]], drop = FALSE],
+#     sbp = slrhsc2.SBP),
+#   n.train = n, n.test = n,
+#   thetahat0 = slrhsc2.a0, thetahat = slrhsc2.thetahat,
+#   betahat = slrhsc2.betahat,
+#   sbp = slrhsc2.SBP,
+#   true.beta = beta, is0.true.beta = is0.beta, non0.true.beta = non0.beta)
+# 
+# # plot the tree given by slr-hsc, indicating significant covariates
+# slrhsc2_leaf_types = rep("covariate", nrow(slrhsc2.SBP))
+# slrhsc2_balance_types = rep("balance", ncol(slrhsc2.SBP))
+# slrhsc2_nodes_types = data.frame(
+#   name = c(colnames(slrhsc2.SBP), rownames(slrhsc2.SBP)),
+#   type = c(slrhsc2_balance_types, slrhsc2_leaf_types)
+# )
+# plotSBP(slrhsc2.SBP, title = "slr-hsc-eta (lasso)", nodes_types = slrhsc2_nodes_types)
+# 
+# # note: centering X and y didn't change the slrSimMat
+# # note 2: using clrXj - clrXk instead of logXj - logXk didn't change slrSimMat
+# 
+# # heat map of cross-validated mse's
+# # fields::image.plot(slrhsc2$cvm)
 
 ##############################################################################
 # Dr. Ma's new slr
@@ -206,7 +206,7 @@ plotSBP(slrhsc2.SBP, title = "slr-hsc-eta (lasso)", nodes_types = slrhsc2_nodes_
 
 # apply supervised log-ratios, using CV to select threshold
 slr.main = slr(
-  y = Y, x = X
+  x = X, y = Y
 )
 
 slrhsc2.eta.min.idx = slrhsc2$min.idx[2]

64c452141263fcd97537727f1ee8254d30209fff
kristynpantoja <kristynp@tamu.edu>
2022-03-30 15:49:24 -0500

    HIV and s sCD14

--- Kristyn/Data/HIV.R
+++ Kristyn/Data/HIV.R
@@ -1,5 +1,5 @@
 # Purpose: compare slr to selbal on data sets
-# Date: 3/29/2022
+# Date: 3/30/2022
 rm(list=ls())
 
 ################################################################################
@@ -98,6 +98,154 @@ slr0_0.5_coefs = getCoefsBM(
   coefs = coefficients(slr0_0.5$model), sbp = slr0_0.5$sbp)
 rownames(slr0_0.5_coefs$llc.coefs)[slr0_0.5_coefs$llc.coefs != 0]
 
+##### cv.slr with approximation step #####
+# slrcv0approx_0.5 = cv.slr(
+#   x = X_0.5, y = Y2, max.clusters = ceiling(sqrt(ncol(X))), nfolds = K,
+#   classification = TRUE, approx = TRUE)
+# saveRDS(
+#   slrcv0approx_0.5,
+#   paste0(
+#     output_dir, "/HIV",
+#     "_slrcv_approx",
+#     "_0.5",
+#     ".rds"))
+slrcv0approx_0.5 = readRDS(
+  paste0(
+    output_dir, "/HIV",
+    "_slrcv_approx",
+    "_0.5",
+    ".rds"))
+slrcv0approx_0.5$nclusters_1se_idx
+slrcv0approx_0.5$nclusters_min_idx
+slrcv0approx_0.5_cvdata = data.frame(
+  `T` = slrcv0approx_0.5$nclusters, 
+  cvm = slrcv0approx_0.5$cvm, 
+  cvse = slrcv0approx_0.5$cvse
+)
+ggplot(slrcv0approx_0.5_cvdata, aes(x = `T`, y = cvm)) + 
+  geom_path() + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = cvm - cvse, ymax = cvm + cvse), width = 0.2) + 
+  scale_x_continuous(breaks = c(slrcv0approx_0.5_cvdata$`T`))
+slrcv0approx_0.5_selected = slrcv0approx_0.5$models[[
+  slrcv0approx_0.5$nclusters_1se_idx]]
+slrcv0approx_0.5_coefs = getCoefsBM(
+  coefs = coefficients(slrcv0approx_0.5_selected$model), 
+  sbp = slrcv0approx_0.5_selected$sbp)
+rownames(slrcv0approx_0.5_coefs$llc.coefs)[
+  slrcv0approx_0.5_coefs$llc.coefs != 0]
+
+##### cv.slr (no approximation step) #####
+# slrcv0_0.5 = cv.slr(
+#   x = X_0.5, y = Y2, max.clusters = ceiling(sqrt(ncol(X))), nfolds = K,
+#   classification = TRUE, approx = FALSE)
+# saveRDS(
+#   slrcv0_0.5,
+#   paste0(
+#     output_dir, "/HIV",
+#     "_slrcv",
+#     "_0.5",
+#     ".rds"))
+slrcv0_0.5 = readRDS(
+  paste0(
+    output_dir, "/HIV",
+    "_slrcv",
+    "_0.5",
+    ".rds"))
+slrcv0_0.5$nclusters_1se_idx
+slrcv0_0.5$nclusters_min_idx
+slrcv0_0.5_cvdata = data.frame(
+  `T` = slrcv0_0.5$nclusters, 
+  cvm = slrcv0_0.5$cvm, 
+  cvse = slrcv0_0.5$cvse
+)
+ggplot(slrcv0_0.5_cvdata, aes(x = `T`, y = cvm)) + 
+  geom_path() + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = cvm - cvse, ymax = cvm + cvse), width = 0.2) + 
+  scale_x_continuous(breaks = c(slrcv0_0.5_cvdata$`T`))
+slrcv0_0.5_selected = slrcv0_0.5$models[[
+  slrcv0_0.5$nclusters_1se_idx]]
+slrcv0_0.5_coefs = getCoefsBM(
+  coefs = coefficients(slrcv0_0.5_selected$model), 
+  sbp = slrcv0_0.5_selected$sbp)
+rownames(slrcv0_0.5_coefs$llc.coefs)[
+  slrcv0_0.5_coefs$llc.coefs != 0]
+
+##### cv.hslr with approximation step #####
+# hslrcv0approx_0.5 = cv.hslr(
+#   x = X_0.5, y = Y2, max.levels = ceiling(sqrt(ncol(X))), nfolds = K,
+#   classification = TRUE, approx = TRUE)
+# saveRDS(
+#   hslrcv0approx_0.5,
+#   paste0(
+#     output_dir, "/HIV",
+#     "_hslrcv_approx",
+#     "_0.5",
+#     ".rds"))
+hslrcv0approx_0.5 = readRDS(
+  paste0(
+    output_dir, "/HIV",
+    "_hslrcv_approx",
+    "_0.5",
+    ".rds"))
+hslrcv0approx_0.5$nclusters_1se_idx
+hslrcv0approx_0.5$nclusters_min_idx
+hslrcv0approx_0.5_cvdata = data.frame(
+  `T` = hslrcv0approx_0.5$nclusters, 
+  cvm = hslrcv0approx_0.5$cvm, 
+  cvse = hslrcv0approx_0.5$cvse
+)
+ggplot(hslrcv0approx_0.5_cvdata, aes(x = `T`, y = cvm)) + 
+  geom_path() + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = cvm - cvse, ymax = cvm + cvse), width = 0.2) + 
+  scale_x_continuous(breaks = c(hslrcv0approx_0.5_cvdata$`T`))
+hslrcv0approx_0.5_selected = hslrcv0approx_0.5$models[[
+  hslrcv0approx_0.5$nclusters_1se_idx]]
+hslrcv0approx_0.5_coefs = getCoefsBM(
+  coefs = coefficients(hslrcv0approx_0.5_selected$model), 
+  sbp = hslrcv0approx_0.5_selected$sbp)
+rownames(hslrcv0approx_0.5_coefs$llc.coefs)[
+  hslrcv0approx_0.5_coefs$llc.coefs != 0]
+
+##### cv.hslr (no approximation step) #####
+# hslrcv0_0.5 = cv.hslr(
+#   x = X_0.5, y = Y2, max.levels = ceiling(sqrt(ncol(X))), nfolds = K,
+#   classification = TRUE, approx = FALSE)
+# saveRDS(
+#   hslrcv0_0.5,
+#   paste0(
+#     output_dir, "/HIV",
+#     "_hslrcv",
+#     "_0.5",
+#     ".rds"))
+hslrcv0_0.5 = readRDS(
+  paste0(
+    output_dir, "/HIV",
+    "_hslrcv",
+    "_0.5",
+    ".rds"))
+hslrcv0_0.5$nclusters_1se_idx
+hslrcv0_0.5$nclusters_min_idx
+hslrcv0_0.5_cvdata = data.frame(
+  `T` = hslrcv0_0.5$nclusters, 
+  cvm = hslrcv0_0.5$cvm, 
+  cvse = hslrcv0_0.5$cvse
+)
+ggplot(hslrcv0_0.5_cvdata, aes(x = `T`, y = cvm)) + 
+  geom_path() + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = cvm - cvse, ymax = cvm + cvse), width = 0.2) + 
+  scale_x_continuous(breaks = c(hslrcv0_0.5_cvdata$`T`))
+hslrcv0_0.5_selected = hslrcv0_0.5$models[[
+  hslrcv0_0.5$nclusters_1se_idx]]
+hslrcv0_0.5_coefs = getCoefsBM(
+  coefs = coefficients(hslrcv0_0.5_selected$model), 
+  sbp = hslrcv0_0.5_selected$sbp)
+rownames(hslrcv0_0.5_coefs$llc.coefs)[
+  hslrcv0_0.5_coefs$llc.coefs != 0]
+
 ##### selbal #####
 # slbl_0.5 = selbal.cv(x = X_0.5, y = Y, n.fold = K)
 # saveRDS(
@@ -165,6 +313,154 @@ slr0_gbm = readRDS(
 slr0_gbm_coefs = getCoefsBM(
   coefs = coefficients(slr0_gbm$model), sbp = slr0_gbm$sbp)
 rownames(slr0_gbm_coefs$llc.coefs)[slr0_gbm_coefs$llc.coefs != 0]
+
+##### cv.slr with approximation step #####
+# slrcv0approx_gbm = cv.slr(
+#   x = X_gbm, y = Y2, max.clusters = ceiling(sqrt(ncol(X))), nfolds = K,
+#   classification = TRUE, approx = TRUE)
+# saveRDS(
+#   slrcv0approx_gbm,
+#   paste0(
+#     output_dir, "/HIV",
+#     "_slrcv_approx",
+#     "_gbm",
+#     ".rds"))
+slrcv0approx_gbm = readRDS(
+  paste0(
+    output_dir, "/HIV",
+    "_slrcv_approx",
+    "_gbm",
+    ".rds"))
+slrcv0approx_gbm$nclusters_1se_idx
+slrcv0approx_gbm$nclusters_min_idx
+slrcv0approx_gbm_cvdata = data.frame(
+  `T` = slrcv0approx_gbm$nclusters, 
+  cvm = slrcv0approx_gbm$cvm, 
+  cvse = slrcv0approx_gbm$cvse
+)
+ggplot(slrcv0approx_gbm_cvdata, aes(x = `T`, y = cvm)) + 
+  geom_path() + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = cvm - cvse, ymax = cvm + cvse), width = 0.2) + 
+  scale_x_continuous(breaks = c(slrcv0approx_gbm_cvdata$`T`))
+slrcv0approx_gbm_selected = slrcv0approx_gbm$models[[
+  slrcv0approx_gbm$nclusters_1se_idx]]
+slrcv0approx_gbm_coefs = getCoefsBM(
+  coefs = coefficients(slrcv0approx_gbm_selected$model), 
+  sbp = slrcv0approx_gbm_selected$sbp)
+rownames(slrcv0approx_gbm_coefs$llc.coefs)[
+  slrcv0approx_gbm_coefs$llc.coefs != 0]
+
+##### cv.slr (no approximation step) #####
+# slrcv0_gbm = cv.slr(
+#   x = X_gbm, y = Y2, max.clusters = ceiling(sqrt(ncol(X))), nfolds = K,
+#   classification = TRUE, approx = FALSE)
+# saveRDS(
+#   slrcv0_gbm,
+#   paste0(
+#     output_dir, "/HIV",
+#     "_slrcv",
+#     "_gbm",
+#     ".rds"))
+slrcv0_gbm = readRDS(
+  paste0(
+    output_dir, "/HIV",
+    "_slrcv",
+    "_gbm",
+    ".rds"))
+slrcv0_gbm$nclusters_1se_idx
+slrcv0_gbm$nclusters_min_idx
+slrcv0_gbm_cvdata = data.frame(
+  `T` = slrcv0_gbm$nclusters, 
+  cvm = slrcv0_gbm$cvm, 
+  cvse = slrcv0_gbm$cvse
+)
+ggplot(slrcv0_gbm_cvdata, aes(x = `T`, y = cvm)) + 
+  geom_path() + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = cvm - cvse, ymax = cvm + cvse), width = 0.2) + 
+  scale_x_continuous(breaks = c(slrcv0_gbm_cvdata$`T`))
+slrcv0_gbm_selected = slrcv0_gbm$models[[
+  slrcv0_gbm$nclusters_1se_idx]]
+slrcv0_gbm_coefs = getCoefsBM(
+  coefs = coefficients(slrcv0_gbm_selected$model), 
+  sbp = slrcv0_gbm_selected$sbp)
+rownames(slrcv0_gbm_coefs$llc.coefs)[
+  slrcv0_gbm_coefs$llc.coefs != 0]
+
+##### cv.hslr with approximation step #####
+# hslrcv0approx_gbm = cv.hslr(
+#   x = X_gbm, y = Y2, max.levels = ceiling(sqrt(ncol(X))), nfolds = K,
+#   classification = TRUE, approx = TRUE)
+# saveRDS(
+#   hslrcv0approx_gbm,
+#   paste0(
+#     output_dir, "/HIV",
+#     "_hslrcv_approx",
+#     "_gbm",
+#     ".rds"))
+hslrcv0approx_gbm = readRDS(
+  paste0(
+    output_dir, "/HIV",
+    "_hslrcv_approx",
+    "_gbm",
+    ".rds"))
+hslrcv0approx_gbm$nclusters_1se_idx
+hslrcv0approx_gbm$nclusters_min_idx
+hslrcv0approx_gbm_cvdata = data.frame(
+  `T` = hslrcv0approx_gbm$nclusters, 
+  cvm = hslrcv0approx_gbm$cvm, 
+  cvse = hslrcv0approx_gbm$cvse
+)
+ggplot(hslrcv0approx_gbm_cvdata, aes(x = `T`, y = cvm)) + 
+  geom_path() + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = cvm - cvse, ymax = cvm + cvse), width = 0.2) + 
+  scale_x_continuous(breaks = c(hslrcv0approx_gbm_cvdata$`T`))
+hslrcv0approx_gbm_selected = hslrcv0approx_gbm$models[[
+  hslrcv0approx_gbm$nclusters_1se_idx]]
+hslrcv0approx_gbm_coefs = getCoefsBM(
+  coefs = coefficients(hslrcv0approx_gbm_selected$model), 
+  sbp = hslrcv0approx_gbm_selected$sbp)
+rownames(hslrcv0approx_gbm_coefs$llc.coefs)[
+  hslrcv0approx_gbm_coefs$llc.coefs != 0]
+
+##### cv.hslr (no approximation step) #####
+# hslrcv0_gbm = cv.hslr(
+#   x = X_gbm, y = Y2, max.levels = ceiling(sqrt(ncol(X))), nfolds = K,
+#   classification = TRUE, approx = FALSE)
+# saveRDS(
+#   hslrcv0_gbm,
+#   paste0(
+#     output_dir, "/HIV",
+#     "_hslrcv",
+#     "_gbm",
+#     ".rds"))
+hslrcv0_gbm = readRDS(
+  paste0(
+    output_dir, "/HIV",
+    "_hslrcv",
+    "_gbm",
+    ".rds"))
+hslrcv0_gbm$nclusters_1se_idx
+hslrcv0_gbm$nclusters_min_idx
+hslrcv0_gbm_cvdata = data.frame(
+  `T` = hslrcv0_gbm$nclusters, 
+  cvm = hslrcv0_gbm$cvm, 
+  cvse = hslrcv0_gbm$cvse
+)
+ggplot(hslrcv0_gbm_cvdata, aes(x = `T`, y = cvm)) + 
+  geom_path() + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = cvm - cvse, ymax = cvm + cvse), width = 0.2) + 
+  scale_x_continuous(breaks = c(hslrcv0_gbm_cvdata$`T`))
+hslrcv0_gbm_selected = hslrcv0_gbm$models[[
+  hslrcv0_gbm$nclusters_1se_idx]]
+hslrcv0_gbm_coefs = getCoefsBM(
+  coefs = coefficients(hslrcv0_gbm_selected$model), 
+  sbp = hslrcv0_gbm_selected$sbp)
+rownames(hslrcv0_gbm_coefs$llc.coefs)[
+  hslrcv0_gbm_coefs$llc.coefs != 0]
 
 ##### selbal #####
 # slbl_gbm = selbal.cv(x = X_gbm, y = Y, n.fold = K)

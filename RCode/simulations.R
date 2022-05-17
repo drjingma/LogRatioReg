8f16dee5b0002e0e0bed77faa73d1863f7e54bb6
drjingma <crystal.jing.ma@gmail.com>
2022-05-17 15:57:10 -0700

    Update simulations: add cross validation with the new SLR algorithm

--- RCode/simulations.R
+++ RCode/simulations.R
@@ -1,208 +1,179 @@
-## Generate parameters
 rm(list=ls())
-jid <- 12
+
+## Last update: 2022-05-13
+today <- '20220513'
+
+jid <- 2
 set.seed(jid)
 # devtools::install_github(repo = "malucalle/selbal")
-# Aim 3: balances regression
-library(philr)
-library(propr)
-library(balance)
+# library(selbal)
+# library(philr)
+# library(propr)
+# library(balance)
 library(tidyverse)
 library(cowplot)
+theme_set(theme_cowplot())
 library(ape)
-library(DiagrammeR)
-# library(selbal)
 library(igraph)
 library(Matrix)
 library(fields)
 library(pheatmap)
+library(ggplot2)
+
+library(DiagrammeR)
 
 CodeFolder <- "RCode/"
 source(paste0(CodeFolder,"func_libs.R"))
-source("RCode/slr-main.R")
+source("RCode/SLR.R")
 
-DEBUG <- 'aim3'
-today <- '20220216'
-linkage <- 'average'   # controls the hclust
-rho <- 0.5 # magnitude of the correlation in cov Sigma
-p <- 50   # number of variables 
+## Generate parameters
+rho <- 0   # magnitude of the correlation in cov Sigma
+p <- 50    # number of variables 
 n <- 100   # number of samples
-sig <- 0.5 # noise level
+sigy <- 0.5   # noise level in regression
+sigx <- 0.5   # noise level in covariates
 r <- 3
 s <- 3
-today <- paste0(DEBUG,'_',today,"_p",p,"_n",n,'_sig',sig*10,'_rho',rho*10)
 
-Sigma <- rgExpDecay(p,rho)$Sigma
-# sds <- rlnorm(p,meanlog = 0.16, sdlog = 0.45)
-# Sigma <- diag(sds) %*% Sigma %*% diag(sds)
-rownames(Sigma) <- colnames(Sigma) <- paste0('s',1:p)
-h_oracle <- hclust(as.dist(1-cov2cor(Sigma)),method = linkage)
-sbp_oracle <- sbp.fromHclust(h_oracle)
 
-## Generate x from normalized log-normal ----
-mu <- c(rep(log(p),5),rep(0,p-5))
-logW <- mvrnorm(n=n*2, mu=mu, Sigma=diag(p)) 
-W <- exp(logW) # basis
-colnames(W) <- paste0('s',1:p)
-x <- sweep(W,1,rowSums(W), FUN='/')
-xCLR <- t(apply(x,1,clr))
-## Generate y ----
-theta <- 1
-# y <-  (rowMeans(log(x[,1:3])) - rowMeans(log(x[,4:6]))) * theta + rnorm(n) * sig
-# y <-  (rowMeans(log(x[,1:4])) - log(x[,5])) * theta + rnorm(2*n) * sig
-
-## Generate x and y from a latent variable model ----
-U <- runif(2*n)
-y <- theta * U + rnorm(2*n) * sig
-xALR <- tcrossprod(as.matrix(U), as.matrix(10*c(rep(1,r)/r,-rep(1,s)/s,rep(0,p-1-r-s)))) + matrix(rnorm(2*n*(p-1)),nrow=2*n) * sig
+# Generate x ----
+U <- runif(2*n,-0.5,0.5)
+alpha <- 10*c(rep(1,r)/r,-rep(1,s)/s,rep(0,p-1-r-s))
+Sigma <- rgExpDecay(p-1,rho)$Sigma
+rownames(Sigma) <- colnames(Sigma) <- paste0('s',1:(p-1))
+xALR <- tcrossprod(as.matrix(U), as.matrix(alpha)) + mvtnorm::rmvnorm(2*n,mean=rep(0,p-1),sigma = Sigma) * sigx
 x <- alrinv(xALR)
+colnames(x) <- paste0('s',1:p)
 
-# y_train <- y[1:n]
-# y_test <- y[-(1:n)]
-# X_train <- x[1:n,]
-# X_test <- x[-c(1:n),]
+## SVD on alr(x) and clr(x) ----
+plot(svd(xALR)$d) # clear that there is one latent variable
+plot(svd(xALR)$v[,1])
+clrx <- t(apply(x,1,clr))
+plot(svd(clrx)$d) # also clear that there is one latent variable
+plot(svd(clrx)$v[,1])
 
-## Model fitting ----
+## Generate y ----
+theta <- 0.25
+y <- theta * U + rnorm(2*n) * sigy
+yb <- 1*(y>0)
+plot(cor(clrx,as.matrix(y)))
+
+## Clustering with the population similarity ----
+popRho <- popGamma(c(alpha,0),theta,sigy^2,sigx^2,U) 
+# between cluster values are not zero
+image.plot(popRho)
+image.plot(max(popRho)-popRho)
+
+W = max(popRho)-popRho
+W.eig <- eigen(W)
+plot(W.eig$values)
+L <- graph.laplacian(W,zeta=0.1)
+L.eig <- eigen(L)
+df <- data.frame(`1`=L.eig$vectors[,1],
+                 `2`=L.eig$vectors[,2],
+                 `3`=L.eig$vectors[,3]) %>%
+  reshape2::melt() %>%
+  ggplot(aes(x=rep(1:p,3),y=value)) + geom_point() +
+  facet_wrap(~variable)
+print(df) 
+
+## Get the empirical similarity matrix ----
+## psiMat is the regression coefficient matrix for continuous response and is equivalent to correlations due to scaling of y
+## rhoMat is the correlation matrix for binary response
 rhoMat <- matrix(0,p,p)
 rownames(rhoMat) <- colnames(rhoMat) <- colnames(x)
-rhoMat2 <- rhoMat
-scale <- 0.1 # controls local behavior
+psiMat <- rhoMat
+AitchisonVar <- rhoMat
 for (j in 1:p){
   for (k in 1:p){
     if (k==j){next}
     else {
-      # testmat[j,k] <- cor.test(log(x[,j])-log(x[,k]),y)$p.value
-      rhoMat[j,k] <- abs(stats::cor(log(x[,j])-log(x[,k]),y))
-      rhoMat2[j,k] <- exp(-(1-rhoMat[j,k])^2/scale)
+      df <- data.frame(y=y,z=log(x[,j])-log(x[,k]))
+      df$z <- scale(df$z)
+      df$y <- df$y - mean(df$y)
+      # df <- as.data.frame(scale(df))
+      psiMat[j,k] <- abs(sum(df$y*df$z)/sum(df$z*df$z))
+      rhoMat[j,k] <- abs(stats::cor(log(x[,j])-log(x[,k]),yb))
+      AitchisonVar[j,k] <- var(log(x[,j])-log(x[,k]))
     }
   }
 }
-diag(rhoMat) <- 1
-# rhoMat <- -0.5 * GowerCentering(rhoMat)
 
-pheatmap(rhoMat,show_colnames = T, show_rownames = T,
+
+pheatmap(AitchisonVar,cluster_rows = F, cluster_cols = F,
+         main='Heatmap of the Aitchison variation')
+
+# Clustering with the empirical similarity ----
+What.psi <- max(psiMat) - psiMat
+pheatmap(What.psi,cluster_rows = F, cluster_cols = F,
+         main='Heatmap of the regression coefficients')
+
+Lhat.psi <- graph.laplacian(What.psi,zeta=0.1)
+Lhat.psi.eig <- eigen(Lhat.psi)
+plot(Lhat.psi.eig$values)
+
+df.psi <- data.frame('1'=Lhat.psi.eig$vectors[,1],
+                     '2'=Lhat.psi.eig$vectors[,2],
+                     '3'=Lhat.psi.eig$vectors[,3]) %>%
+  reshape2::melt() %>%
+  ggplot(aes(x=rep(1:p,3),y=value)) + geom_point() +
+  facet_wrap(~variable)
+print(df.psi)
+kmeans(Lhat.psi.eig$vectors[,1:3],centers=3,nstart=10)$cluster
+
+
+What <- max(rhoMat) - rhoMat
+pheatmap(What,show_colnames = T, show_rownames = T,
          cluster_rows = F, cluster_cols = F,
          main = 'Heatmap of the original correlation')
-spectral.clustering(rhoMat)
-# pheatmap(rhoMat2,show_colnames = T, show_rownames = T,
-#          cluster_rows = F, cluster_cols = F,
-#          main = 'Heatmap of the radial similarity')
-# a = spectral.clustering(rhoMat2) # this won't work, because the points are in two clusters
-# which(a==1)
-# which(a==-1)
-rhoMat.svd <- svd(rhoMat)
-plot(rhoMat.svd$d)
 
-list.approx <- lapply(1:p, function(j) tcrossprod(rhoMat.svd$u[,j], rhoMat.svd$v[,j]) * rhoMat.svd$d[j])
-rhoMat_approx_1 <-  Reduce("+",list.approx[1:2])
-b = spectral.clustering(rhoMat_approx_1)
-index <- which(b==1)
-which(b==-1)
-# pheatmap(rhoMat_approx_1,show_colnames = T, show_rownames = T,
-#          cluster_rows = F, cluster_cols = F,
-#          main = 'Heatmap of the Rank-1 approximated similarity')
+What.eig <- eigen(What)
+Lhat <- graph.laplacian(What,zeta=0.1)
+Lhat.eig <- eigen(Lhat)
+plot(Lhat.eig$values)
 
-## Perform spectral clustering on each subset of variables using the original correlation matrix
-# subset1 <- spectral.clustering(rhoMat2[index,index])
-# subset2 <- spectral.clustering(rhoMat2[-index,-index])
+dfhat <- data.frame('1'=Lhat.eig$vectors[,1],
+                    '2'=Lhat.eig$vectors[,2],
+                    '3'=Lhat.eig$vectors[,3]) %>%
+  reshape2::melt() %>%
+  ggplot(aes(x=rep(1:p,3),y=value)) + geom_point() +
+  facet_wrap(~variable)
+print(dfhat)
 
-# out <- slr(x,y)
-# head(sort(apply(rhoMat,1,sum), decreasing = T))
-# setdiff(names(head(sort(apply(rhoMat,1,max), decreasing = T))),names(index))
-# setdiff(names(head(sort(apply(rhoMat,1,sum), decreasing = T))),names(index))
-# a <- apply(rhoMat,1,max)
-# b <- apply(rhoMat,1,sum)
-# a <- a/max(a)
-# b <- b/max(b)
-# alpha <- 1
-# head(sort(alpha*a + (1-alpha)*b,decreasing = T))
+cl <- kmeans(Lhat.eig$vectors[,1:3],centers=3,nstart=10)$cluster
+# kmeans(Lhat.eig$vectors[,1:2],centers=2,nstart=10)$cluster
 
-# rhoMat.svd <- svd(rhoMat)
-# rhoMat_approx_1 <-  tcrossprod(rhoMat.svd$u[,1], rhoMat.svd$v[,1]) * rhoMat.svd$d[1]
-# rownames(rhoMat_approx_1) <- colnames(rhoMat_approx_1) <- rownames(rhoMat)
-# pheatmap(rhoMat_approx_1,show_colnames = T, show_rownames = T, cluster_rows = F, cluster_cols = F,
-#          main = 'Heatmap of first SVD approximated correlation')
-# cl <- spectral.clustering(rhoMat_approx_1)
-# index <- which(cl==1)
-# spectral.clustering(rhoMat[index,index])
 
-# setdiff(names(head(sort(apply(rhoMat_approx_1,1,max), decreasing = T),n=sum(abs(sbp_oracle[,jid])))),names(index))
-# setdiff(names(head(sort(apply(rhoMat_approx_1,1,sum), decreasing = T),n=sum(abs(sbp_oracle[,jid])))),names(index))
-# 
-# rhoMat_approx_2 <-  rhoMat.svd$u[,1:2] %*% diag(rhoMat.svd$d[1:2]) %*% t(rhoMat.svd$v[,1:2])
-# rownames(rhoMat_approx_2) <- colnames(rhoMat_approx_2) <- rownames(rhoMat)
-# pheatmap(rhoMat_approx_2,show_colnames = T, show_rownames = T, cluster_rows = F, cluster_cols = F)
-# spectral.clustering(rhoMat_approx_2)
-# head(sort(apply(rhoMat_approx_2,1,max), decreasing = T))
-# head(sort(apply(rhoMat_approx_2,1,sum), decreasing = T))
+## Build balance from these three subsets
+SBP <- matrix(0,nrow=length(cl),ncol=3)
+SBP[(cl==1),1] <- 1
+SBP[(cl==2),1] <- -1
+SBP[(cl==1),2] <- 1
+SBP[(cl==3),2] <- -1
+SBP[(cl==2),3] <- 1
+SBP[(cl==3),3] <- -1
+rownames(SBP) <- colnames(x)
+z <- balance::balance.fromSBP(x,SBP)
+cor(z,yb)
+apply(abs(SBP),2,sum)
 
-# rho4 <- rhoMat_approx_1
-# rho4[which(rho4<0.55)] <- 0
-# pheatmap(rho4,show_colnames = T, show_rownames = T, cluster_rows = F, cluster_cols = F)
-# rho4 <- rhoMat
-# rho4[which(rho4<0.27)] <- 0
-# plot(hclust(as.dist(1-rhoMat)))
 
-# spectral.clustering(rhoMat)
+# object <- cv.slr(x,y,method='correlation',response.type = 'continuous', threshold = NULL,s0.perc = 0)
+# plot(object$threshold)
+# plot(object$cvm)
+# object$threshold.1se
+# object$threshold.min
+# fit <- slr(x,y,method='correlation',response.type = 'continuous',threshold = object$threshold.min,s0.perc = 0)
+# fit$sbp
 
-# library(pROC)
-# library(ggplot2)
-# source("RCode/Selbal_Functions.R")
-# out <- selbal(x,yAll)
+object.wald <- cv.slr(x,y,method='wald',response.type = 'continuous', threshold = NULL,s0.perc = 0)
+plot(object.wald$threshold)
+plot(object.wald$cvm)
+object.wald$threshold.1se
+object.wald$threshold.min
+fit.wald <- slr(x,y,method='wald',response.type = 'continuous',threshold = object.wald$threshold.1se,s0.perc = 0)
+fit.wald$sbp
 
-## ---- Set the error matrix ----
-# err <- matrix(NA,nrow=6,ncol=2)
-# rownames(err) <- c('slr','propr','coat','pba','classo','selbal')
-# colnames(err) <- c('training', 'test')
-# x = X_train; y=y_train; lambda=seq(0.1,0.7,0.02)
-# fit <- cv.slr(x = X_train, y=y_train, lambda=seq(0.3,0.7,0.02))
-# refit <- slr(X_train, y_train, lambda=0.75)
-# err[1,1] <- mean((y_train - refit$fitted.values)^2)
-# err[1,2] <- mean((y_test - predict(refit,X_test))^2)
-## Tuning parameters
+# object.wald <- cv.slr(x,yb,method='wald',response.type = 'binary', threshold = NULL,s0.perc = 0)
+# fit.wald <- slr(x,yb,method='wald',response.type = 'binary', threshold = object.wald$threshold.1se,s0.perc = 0)
 
-# ## Construct SBP from proportionality matrix
-# pr <- propr::propr(x[1:n,], metric = "phs")
-# sbp_propr <- sbp.fromHclust(hclust(as.dist(pr@matrix),method = linkage))
-# ba_propr <- balance.fromSBP(x,sbp_propr)
-# fit_propr <- run.glmnet(x=ba_propr[1:n,],y,xt=ba_propr[-(1:n),],y_test,lambda = lambda)
-# err[2,1] <- fit_propr$mse.pred
-# roc_propr <- apply(fit_propr$beta, 2, function(a) roc.for.coef.LR(a, beta_lc, sbp_propr))
-# 
-# # d_coat <- 1-coat(x[1:n,])$corr
-# # rownames(d_coat) <- colnames(d_coat) <- colnames(W)
-# # sbp_coat <- sbp.fromHclust(hclust(as.dist(d_coat),method = linkage))
-# # ba_coat <- balance.fromSBP(x,sbp_coat)
-# # fit_coat <- run.glmnet(x=ba_coat[1:n,],y,xt=ba_coat[-(1:n),],y_test)
-# # err[3,1] <- fit_coat$mse.pred
-# # roc_coat <- apply(fit_coat$beta, 2, function(a) roc.for.coef.LR(a, beta_lc, sbp_coat))
-# 
-# ## Construct principal balances
-# sbp_pba <- pba(x)
-# fit_pba <- run.glmnet(x=sbp_pba@pba[1:n,],y,xt=sbp_pba@pba[-(1:n),],y_test,lambda = lambda)
-# err[4,1] <- fit_pba$mse.pred
-# roc_pba <- apply(fit_pba$beta, 2, function(a) roc.for.coef.LR(a, beta_lc, sbp_pba@sbp))
-# 
-## Fit compositional Lasso using the same lambda
-# nlam <- 100
-# maxlam <- 2*max(abs(crossprod(log(X_train),y_train)/n))
-# lambda <- exp(seq(from=log(maxlam), to=log(1e-4), length.out=nlam))
-# fit <- ConstrLasso(y=y_train,x=log(X_train),Cmat=matrix(1,p,1))
-# fit_classo <- cv.func('ConstrLasso',y=y_train,x=log(X_train),Cmat=matrix(1,p,1), lambda = lambda, nfolds=10)
-# pred_classo <- fit_classo$int[which.min(fit_classo$cvm)] + log(X_test) %*% fit_classo$bet[,which.min(fit_classo$cvm)]
-# err[5,2] <- mean((pred_classo - y_test)^2)
-# err[5,1] <- mean((fit_classo$int[which.min(fit_classo$cvm)] + log(X_train) %*% fit_classo$bet[,which.min(fit_classo$cvm)] - y_train)^2)
-# roc_classo <- apply(fit_classo$bet, 2, function(a) roc.for.coef(a,beta_lc))
-# 
-# print(err)
-# plot(roc_supervised[1,],roc_supervised[2,],type='b')
-# lines(roc_classo[1,],roc_classo[2,],col='red')
-# 
-# res <- list(sup=roc_supervised,
-#             pba=roc_pba,
-#             propr=roc_propr,
-#             classo=roc_classo,
-#             err=err, beta=beta_lc, mu=mu)
-# 
-# if (!is.null(res)) {save(res, file=paste0(OutFolder, today, '_',jid,'.rda'))}
-# 

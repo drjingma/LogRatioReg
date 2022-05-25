ce2f37a1f1487dd2c4f28bc829fdbc1fbc1c4b73
Patrick <patrickding00@gmail.com>
2022-05-25 10:20:43 -0500

    sims

--- Kristyn/Experiments/current_experiments/sims_latent.R
+++ Kristyn/Experiments/current_experiments/sims_latent.R
@@ -59,8 +59,8 @@ res = foreach(
   tol = 1e-4
   sigma_eps1 = 0.1 # sigma (for y)
   sigma_eps2 = 0.1 # sigma_j (for x)
-  # SBP.true = matrix(c(1, 1, 1, -1, -1, -1, rep(0, p - 6)))
-  SBP.true = matrix(c(1, 1, 1, 1, -1, rep(0, p - 5)))
+  SBP.true = matrix(c(1, 1, 1, -1, -1, -1, rep(0, p - 6)))
+  # SBP.true = matrix(c(1, 1, 1, 1, -1, rep(0, p - 5)))
   ilrtrans.true = getIlrTrans(sbp = SBP.true, detailed = TRUE)
   # ilrtrans.true$ilr.trans = transformation matrix (used to be called U) 
   #   = ilr.const*c(1/k+,1/k+,1/k+,1/k-,1/k-,1/k-,0,...,0)

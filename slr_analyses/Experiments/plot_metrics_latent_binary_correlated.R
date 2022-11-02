95be8882253b93094863009817d2606e442be05a
kristynpantoja <kristynp@tamu.edu>
2022-11-02 17:46:41 -0400

    corr bin plts

--- slr_analyses/Experiments/plot_metrics_latent_binary_correlated.R
+++ slr_analyses/Experiments/plot_metrics_latent_binary_correlated.R
@@ -20,7 +20,7 @@ library(ggrepel)
 numSims = 100 
 
 # Settings to toggle with
-settings.name = "BinaryResponse"
+settings.name = "CorrBinaryResponse"
 hparam = "1se"
 n = 100
 p = 30
@@ -31,8 +31,8 @@ intercept = TRUE
 scaling = TRUE
 tol = 1e-4
 sigma_x = 0.1
-SBP.true = matrix(c(1, 1, 1, -1, -1, -1, rep(0, p - 6)))
-# SBP.true = matrix(c(1, 1, 1, 1, -1, rep(0, p - 5)))
+# SBP.true = matrix(c(1, 1, 1, -1, -1, -1, rep(0, p - 6)))
+SBP.true = matrix(c(1, 1, 1, 1, -1, rep(0, p - 5)))
 ilrtrans.true = getIlrTrans(sbp = SBP.true, detailed = TRUE)
 # ilrtrans.true$ilr.trans = transformation matrix (used to be called U) 
 #   = ilr.const*c(1/k+,1/k+,1/k+,1/k-,1/k-,1/k-,0,...,0)

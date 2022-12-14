b8a284cf7780b7b7c302a9c0c5510cdc10782a9d
kristynpantoja <kristynp@tamu.edu>
2022-12-14 13:34:42 -0500

    simscor, simsbin, simsbincor 1234v56

--- Experiments/sims_latent_binary_correlated.R
+++ Experiments/sims_latent_binary_correlated.R
@@ -56,12 +56,13 @@ res = foreach(
   scaling = TRUE
   sigma_x = 0.1
   # SBP.true = matrix(c(1, 1, 1, -1, -1, -1, rep(0, p - 6)))
-  SBP.true = matrix(c(1, 1, 1, 1, -1, rep(0, p - 5)))
+  # SBP.true = matrix(c(1, 1, 1, 1, -1, rep(0, p - 5)))
+  SBP.true = matrix(c(1, 1, 1, 1, -1, -1, rep(0, p - 6)))
   ilrtrans.true = getIlrTrans(sbp = SBP.true, detailed = TRUE)
   # ilrtrans.true$ilr.trans = transformation matrix (used to be called U) 
   #   = ilr.const*c(1/k+,1/k+,1/k+,1/k-,1/k-,1/k-,0,...,0)
   b0 = 0 # 0
-  b1 = 10 # 6, 10
+  b1 = 6 # 6, 10
   c.value = 1 # a1 = c.value / k+ or c.value / k- or 0
   a0 = 0 # 0
   ulimit = 0.5
